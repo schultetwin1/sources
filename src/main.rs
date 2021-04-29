f4faae91c11c3d8b8239b6ae95642f528b5e005a
Matt Schulte <mschulte@wyze.com>
2021-04-28 20:22:52 -0700

    Clean up paths

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 
-use std::path::PathBuf;
+use std::path::{Component, Path, PathBuf};
 
 fn main() -> Result<()> {
     let matches = clap::App::new(env!("CARGO_PKG_NAME"))
@@ -27,7 +27,45 @@ fn main() -> Result<()> {
 
     let files = compiledfiles::parse(input_file)?;
     for file in files {
-        println!("{}", file.path.display());
+        let mut path = file.path;
+        if let Ok(cwd) = std::env::current_dir() {
+            if path.is_absolute() && path.starts_with(&cwd) {
+                path = path.strip_prefix(&cwd).unwrap().to_path_buf();
+            }
+        }
+        path = normalize_path(&path);
+        println!("{}", path.display());
     }
     Ok(())
+}
+
+// copied from https://github.com/rust-lang/cargo/blob/2e4cfc2b7d43328b207879228a2ca7d427d188bb/src/cargo/util/paths.rs#L65-L90
+// both projects are MIT https://github.com/rust-lang/cargo/blob/master/LICENSE-MIT
+// for std impl progress see rfc https://github.com/rust-lang/rfcs/issues/2208
+// replace this once that lands
+pub fn normalize_path(path: &Path) -> PathBuf {
+    let mut components = path.components().peekable();
+    let mut ret = if let Some(c @ Component::Prefix(..)) = components.peek().cloned() {
+        components.next();
+        PathBuf::from(c.as_os_str())
+    } else {
+        PathBuf::new()
+    };
+
+    for component in components {
+        match component {
+            Component::Prefix(..) => unreachable!(),
+            Component::RootDir => {
+                ret.push(component.as_os_str());
+            }
+            Component::CurDir => {}
+            Component::ParentDir => {
+                ret.pop();
+            }
+            Component::Normal(c) => {
+                ret.push(c);
+            }
+        }
+    }
+    ret
 }
